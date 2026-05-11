f1b2e24a6fe0c878f9c4a7755d7bb6579f96c87f
Temametr <122975020+Temametr@users.noreply.github.com>
2026-05-11 16:28:59 +0300

    Update pollingManager.js

--- js/core/polling/pollingManager.js
+++ js/core/polling/pollingManager.js
@@ -1,6 +1,7 @@
 import { polling } from '../../state.js';
+import { APP_CONFIG } from '../../config/appConfig.js';
 
-export function startPolling(callback, intervalMs = 15000) {
+export function startPolling(callback, intervalMs = APP_CONFIG.polling.bookingsInterval) {
     stopPolling();
 
     polling.interval = setInterval(callback, intervalMs);
