30090282dc84cc31a997e08950fca29bb69972c6
Temametr <122975020+Temametr@users.noreply.github.com>
2026-05-11 16:26:58 +0300

    Create appConfig.js

---
+++ js/config/appConfig.js
@@ -0,0 +1,18 @@
+export const APP_CONFIG = {
+    polling: {
+        bookingsInterval: 15000
+    },
+
+    booking: {
+        defaultDuration: 60
+    },
+
+    map: {
+        lat: 50.027388,
+        lng: 36.3314636
+    },
+
+    ui: {
+        animationDuration: 300
+    }
+};
